1afe4fe4cd92ebbe867a541e1304cff6c78a2a1d
David <54381677+DLis6@users.noreply.github.com>
2019-10-09 19:45:44 -0500

    TareasReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      list: []
+    }
+
+    this.text = this.text.bind(this);
+    this.keyPress = this.keyPress.bind(this);
+  };
+
+  text(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  keyPress(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+     this.setState({
+       list: this.state.list.concat([this.state.value]),
+       value:""
+     })
+    
+    }
+  }
+
   render() {
+    
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
-          <ul className="todo">
+          <ul className="todo" >
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.list.map(elementList => {
+            return <li key={`elementList_${elementList}`}>{elementList}</li>;
+          })}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form>
+            <input
+              type="text"
+              id="new-task"
+              placeholder="Ingresa una tarea y oprime Enter"
+              value={this.state.value}
+              onKeyPress={this.keyPress}
+              onChange={this.text} 
+            />
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
